1d20d6b1cc9856417de7c10577184e3515065aa1
Vijay shelke <74587203+vijay26-10@users.noreply.github.com>
2023-01-10 19:36:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 //your code here
+$(document).ready(function() {
+  // Fetch the initial user's data
+  fetchUser();
+
+  // Show additional info when a button is clicked
+  $("#additionalInfo").on("click", "button", function() {
+    var attr = $(this).data("attr");
+    $("#info").empty();
+    $("#info").append(userData[attr]);
+  });
+
+  // Fetch a new user's data when the "Get New User" button is clicked
+  $("#getUser").on("click", function() {
+    fetchUser();
+  });
+});
+
+var userData = {};
+
+function fetchUser() {
+  $.ajax({
+    url: "https://randomuser.me/api/",
+    dataType: "json",
+    success: function(data) {
+      var user = data.results[0];
+      userData = {
+        "name": user.name.first + " " + user.name.last,
+        "photo": user.picture.large,
+        "age": user.dob.age,
+        "email": user.email,
+        "phone": user.phone
+      };
+
+      $("#name").html(userData.name);
+      $("#photo").attr("src", userData.photo);
+    }
+  });
+}
